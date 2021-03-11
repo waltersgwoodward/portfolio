2b9c3b4d2435e2b09a32c6f58508d044a7b56874
walterswoodward <walter.sekope.woodward@gmail.com>
2021-03-11 12:20:53 -0800

    Adjust html tstructure in About.js to ensure links are only clickable where expected

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -9,16 +9,20 @@ class About extends PureComponent {
             <div className='page'>
                 <div className='page__row page-row__name'>WALTER WOODWARD</div>
                 <div className='page__row page-row__title'>FULL STACK WEB DEVELOPER</div>
-                <Link
-                    className='page__row page-row__link'
-                    to='/portfolio'
-                    aria-label="Click here to view Walter's current projects"
-                >PORTFOLIO</Link>
-                <Link
-                    className='page__row page-row__link'
-                    to='/contact'
-                    aria-label="Click here to view Walter's Contact form"
-                >CONTACT</Link>
+                <div className='page__row page-row__link'>
+                    <Link
+                        className='page-row__link'
+                        to='/portfolio'
+                        aria-label="Click here to view Walter's current projects"
+                    >PORTFOLIO</Link>
+                </div>
+                <div className='page__row page-row__link'>
+                    <Link
+                        className='page-row__link'
+                        to='/contact'
+                        aria-label="Click here to view Walter's Contact form"
+                    >CONTACT</Link>
+                </div>
                 <div className="page__row page-row__link">
                     <ContactLinks />
                 </div>
