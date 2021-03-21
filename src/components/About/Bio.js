9269f201e531733266864aec49f2d8a493eef133
walterswoodward <walter.sekope.woodward@gmail.com>
2021-03-20 21:57:38 -0700

    Bio Update

--- src/components/About/Bio.js
+++ src/components/About/Bio.js
@@ -6,24 +6,6 @@ class Bio extends PureComponent {
         return (
             <Fragment>
                 <div className='page'>
-                    <div className='page-paragraph__wrap'>
-                        <div className="page-paragraph__header">Before Programming</div>
-                        <div className="page__paragraph">I grew up in and around Boston, MA. I love music, which led me to pursuing a Bachelor of Music at McGill University.
-After McGill I spent a lot of time in academia searching for something that I could really see myself committing to.
-Among other things I considered sound engineering, accounting, organic farming, and plant pathology.
-During this time I also served as an Army Reserve Bandsman with the 215th Army Band in Fall River, MA for six years.
-                        </div>
-                    </div>
-                    <div className='page-paragraph__wrap'>
-                        <div className="page-paragraph__header">Coming to Faith</div>
-                        <div className="page__paragraph">Following my grandfather's death in 2004, I became very interested in the Bible and the Christian faith.
-I was baptized in 2013, and with the support of my home church enrolled in the Master of Arts in Theology program at Gordon Conwell Theological Seminary the following year.
-In my final semester (fall of 2017) of seminary, I came to the conclusion that pursuing ministry in a professional or academic setting was not the path for me.
-Despite my commitment to the Christian faith, it was clear that the gifts God had given me were more in the direction of engineering and less in the direction of a community/spiritual leader.
-I took a few online courses through Code Academy and Udacity, and discovered that I had a genuine passion for programming.
-The more I thought about it, the more it made sense to pursue software engineering, and so I decided to enroll in a coding bootcamp.
-                        </div>
-                    </div>
                     <div className="page-paragraph__wrap">
                         <div className="page-paragraph__header">Coding Education</div>
                         <div className="page__paragraph">From January to August 2018, I attended Lambda School's <a className="cards-header__link " href="https://lambdaschool.com/courses/full-stack-web-development">Full Stack Web Development program</a>.
@@ -43,7 +25,6 @@ What attracted me to Scan123 was being able to work on a team, rather than just 
 Futhermore, the lead developer was adamant about limiting the introduction of third party dependencies, which if you are trying to establish a long term sustainable SaaS, is essential.
 During my time at Scan123, I contributed code to every level of the stack; from UI/UX (HTML,CSS, JavaScript), to authentication and validation of user input (PHP), to building and revising our own mySQL query builder.
 After fifteen months with Scan123, I felt it was time for me to seek new opportunities.
-Having worked extensively within the LEMP stack, I wanted to commit more time to investigating more popular web development solutions like AWS and Python.
                         </div>
                     </div>
                     <div className="page-paragraph__wrap">
