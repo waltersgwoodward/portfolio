d3a0853cb361097b71304a0ba45c07edec792067
walterswoodward <walter.sekope.woodward@gmail.com>
2021-04-02 12:01:52 -0700

    Update bio

--- src/components/About/Bio.js
+++ src/components/About/Bio.js
@@ -8,6 +8,28 @@ class Bio extends PureComponent {
         return (
             <Fragment>
                 <div className='page'>
+                <div className="page-paragraph__wrap">
+                        <div className="page-paragraph__header">Faith</div>
+                        <div className="page__paragraph">At my core, I understand the world through a biblical lens.
+Working in the tech industry, I have come to accept that this makes me a bit of a rare breed. For that reason, I offer this short defense for my faith, and explanation for why I don't believe it to be in conflict with most work environments.
+                        </div>
+                        <br/>
+                        <div className="page__paragraph">The bible is interpretted and used in a merid of ways, and certainly has been at the center of countless evils in this world.
+                            <div className="page__paragraph">Yet, within its pages you can find these words:
+                                <div className="page__paragraph highlight__text">The entire law is fulfilled in a single decree: "Love your neighbor as yourself" (Galatians 5:14)</div>
+                                <div className="page__paragraph highlight__text">Above all, love one another deeply, because love covers over a multitude of sins." (1 Peter 4:8)</div>
+                            </div>
+                        </div>
+                        <div className="page__paragraph">I'm not going to pretend that there aren't what appear to be an abundance of issues in the bible. 
+Only that for me, the above two passages are what I mean when I say, "I am a Christian".
+With regard to work, I work because it is something I believe I was designed to do.
+Jesus says, <span class="highlight__text">"Man shall not live by bread alone, but by every word that proceedeth out of the mouth of God."</span>&nbsp;
+Though this applies to more than just work, I believe that Adam and Eve's occupation as gardeners before the fall, together with our own experience, both testify to the truth that work and the dignity it provides, were always intended to be a good thing for mankind to enjoy.
+We are nevertheless burdened by many fears, which drive us apart from one another, even ourselves, and make working together, at times, incredibly difficult.
+It is my reliance on and belief in God that has been and continues to be my guide in navigating through these challenges.
+I have found that this has only made me more of an asset whereever I have worked.
+                        </div>
+                    </div>
                     <div className="page-paragraph__wrap">
                         <div className="page-paragraph__header">Coding Education</div>
                         <div className="page__paragraph">From January to August 2018, I attended Lambda School's <BioLink href="https://lambdaschool.com/courses/full-stack-web-development" title="Full Stack Web Development program"/>.
@@ -31,9 +53,9 @@ The project was very rewarding as it required solving scalability and performanc
                     <div className="page-paragraph__wrap">
                         <div className="page-paragraph__header">Leaving Scan123</div>
                         <div className="page__paragraph">
-As much as I enjoyed the work, I eventually had to accept that I would not be able to sustain myself at the pace that Scan123 required, if I wished to maintain a life outside of work.
-Long story short, after fifteen months with Scan123, I felt it was time for me to make a new plan.
-One that would be compatible with my desire to have time for my wife, our kids, and myself.
+As much as I enjoyed the work, I eventually had to accept that I would not be able to sustain myself at the pace that Scan123 required.
+Long story short, my wife and I want to settle down and have children, and so having occupations that support that, specifically by encouraging a healthy work/life balance is essential.
+After fifteen months with Scan123, I felt that while the work was fulfilling and the compensation more than adequate, what I really needed was a stable 9 to 5 that would allow me to focus on my family while also sustaining us financially.
                         </div>
                     </div>
                     <div className="page-paragraph__wrap">
