fcbc837c574fe23aa8a407ea6588336a5b10ed15
walterswoodward <walter.sekope.woodward@gmail.com>
2021-06-03 11:20:01 -0700

    Bio Update

--- src/components/About/Bio.js
+++ src/components/About/Bio.js
@@ -36,7 +36,7 @@ But whoever does what is true comes to the light, so that it may be clearly seen
                         </div>
                         <div className="page__paragraph">Of course, these are hard truths to accept.
 According to the scriptures, impossible, without divine intervention (1 Corinthians 2:14).
-Though most will disagree, I would submit that the message of the Bible is indeed one of hope, healing, and love.
+I would submit that the message of the Bible is indeed one of hope, healing, and love.
 I maintain that belief while acknowledging that the symbols, banners, and cultures we associate with Christianity have been and continue to be used, more often than not, as tools to enslave, rather than liberate.
 The word of God cuts through all of that and points to something deeper, something living, and something freely available to you and me:
                             <div className="page__paragraph highlight__text">Come to me, all you who are weary and burdened, and I will give you rest.
@@ -52,7 +52,7 @@ I graduated with a basic understanding of JavaScript, some general familiarity w
 I got my first job working with <BioLink href="https://www.theatomgroup.com/" title="The Atom Group"/> in Portsmouth, NH in December 2018.
 The job was a contract position working alongside developers at IBM on their E2E testing framework.
 When I arrived at The Atom Group, we were introduced to a Node.js, Selenium, and NightWatchJS E2E testing application that had been built by a recent CS graduate.
-The structure for the application was there, but many edge cases had not been addressed, libraries utilized were out of date, the code itself was pratically unreadable, and there was no documentation.
+The structure for the application was there, but many edge cases had not been addressed, libraries utilized were out of date, the code itself was practically unreadable, and there was no documentation.
 In my time with The Atom Group I worked with a team to address all of the above, leaving them with a working application, complete with a Getting Started guide for future developers.
 <br/><br/>
 Nearing the end of my contract with The Atom Group, I was fortunate to find a job working with a small ecommerce company called <BioLink href="https://www.handicappedpets.com/" title="HandicappedPets.com"/>.
@@ -62,15 +62,17 @@ Although it has been well over a year since I left HandicappedPets, the SureFit 
 <br/><br/>
 In November 2019, my wife and I moved out to Portland, OR, and soon after our arrival I accepted a position as a software developer at a local SaaS Document Management company called <BioLink href="https://scan123.com/" title="Scan123"/>.
 During my time at Scan123, I worked in an Agile Scrum environment and contributed code to every level of the stack; from UI/UX (HTML,CSS, JavaScript), to authentication and validation of user input (PHP), to building and revising our own mySQL query builder.
-My largest contribution to Scan123 was building a sphinx search microservice capable of providing real-time search results of over 150 million documents.
-The project was very rewarding as it required solving scalability and performance issues in creative ways while keeping costs to a minimum.
+My largest contribution to Scan123 was resurrecting an outdated, increasingly underutilized search user interface that crashed and stalled under the weight of Scan123's growing database.
+After deciding that we would continue to utilize the open source tool Sphinx Search as the search engine upon which to build our search microservice, I set out to familiarize myself with the technology.
+After about a month, I was granted a couple developers to help me build out a completely new search feature involving drafting new search ui/ux designs, getting buy-in from customers through interviews, adjusting to their feedback, and finally building out the product.
+In the end we were able to meet our deadlines and successfully build out a search microservice capable of providing real-time search results of over 150 million documents combined with an improved search user interface that allowed customers to conduct advanced search queries of their documents using Optical Character Recognition (generated using Machine Learning powered by Tesseract), identifiers unique to the automotive industry, as well as common search terms such as date ranges, keywords, and parent folder/cabinet names. 
 After fifteen months at Scan123, I decided it was time for a change, and on February 1st, 2021 sent in my resignation letter.
                         </div>
                     </div>
                     <div className="page-paragraph__wrap">
                         <div className="page-paragraph__header">Current Activity</div>
                         <div className="page__paragraph">
-I feel a personal responsibility to seek roles that make a positive difference in people's lives.
+I am passionate about technology and the benefits of automation, but continue to feel a personal responsibility to seek roles that make a positive difference in people's lives.
 I am currently looking for a long term, full time position in (or around) the Portland area that I can commit to, and that will support my desire to put down roots and start a family.
 In the meantime, I have decided to make a commitment to volunteering at the Portland Rescue Mission; a community that helps homeless people get back on their feet.
 Working at PRM and witnessing the generosity of staff and volunteers helping those most in need brings me great joy.
